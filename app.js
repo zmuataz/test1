afe73aa70d5c3802baefae2b40f2fc785e22c30a
Azlan Mukhtar <azlan.mukhtar@gmail.com>
2017-12-18 16:28:51 +0800

    add 1 more line

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var http= require('http');
 var s=http.createServer(function(req,res){
     res.writeHead(200, {'content-type': 'text/plain'});
     res.write("hello  test\n");
+	res.write('more line');
     setTimeout(function(){
     res.end("World\n");
 
