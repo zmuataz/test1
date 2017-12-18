791861922d2cf99b76ca2c38023f399b73921e95
zubaidah muataz <zmuataz@gmail.com>
2017-12-18 16:03:14 +0800

    change 'hello' to something

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http= require('http');
 var s=http.createServer(function(req,res){
     res.writeHead(200, {'content-type': 'text/plain'});
-    res.write("hello\n");
+    res.write("hello  test\n");
     setTimeout(function(){
     res.end("World\n");
 
